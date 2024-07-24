f311d2f8bcb93d646a525a7e3ecf641764f25757
Pavlov.SA3 <pavlov.sa3@dns-shop.ru>
2024-07-24 17:58:25 +1000

    Remake full project

--- Code/ddl/Text.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-data_with={'token':'27993.NNBgQ3UdDg618ydAg',
-      'qrraw':'t=20240714T1608&s=301.85&fn=7380440700428408&i=17605&fp=477946500&n=1'}
-response = requests.post(f"https://proverkacheka.com/api/v1/check/get", data=data_with)
-json_obj = response.json()['data']['json']
-for obj in json_obj:
-    print(obj)
-
