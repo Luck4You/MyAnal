11fb993dc03e5d46d266b6302d6db43cd9f4c6bb
Pavlov.SA3 <pavlov.sa3@dns-shop.ru>
2024-07-26 13:23:08 +1000

    ReChangedBot

--- tg_bot/Bot.py
+++ tg_bot/Bot.py
@@ -4,7 +4,7 @@ import telebot
 
 
 
-bot = telebot.TeleBot("7088145772:AAH-xwQGCWjHttMfA9hdNC9cFk29rkKymAs")
+bot = telebot.TeleBot("7088145772:AAE7q550EfrV4iofg_v8ow1FEsA9NNtmfeM")
 
 @bot.message_handler(content_types=["text"])
 def start(message):
@@ -17,7 +17,5 @@ def foo(message):
     dict_with = je.send_request_to_dict(message.text)
     for obj in dict_with:
         bot.send_message(message.chat.id, f"Покупка: {obj}") 
-    
+        
 bot.polling(none_stop=True)
-
-
