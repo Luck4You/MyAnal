f69ecceb7a20b8be9661fa3b5a0ea71be262ff17
Pavlov.SA3 <pavlov.sa3@dns-shop.ru>
2024-07-26 14:04:19 +1000

    Rebalance_Bot

--- tg_bot/Bot.py
+++ tg_bot/Bot.py
@@ -9,10 +9,11 @@ bot = telebot.TeleBot("7088145772:AAE7q550EfrV4iofg_v8ow1FEsA9NNtmfeM")
 @bot.message_handler(content_types=["text"])
 def start(message):
     if  message.text.lower() == "/start":
-        bot.send_message(message.chat.id, bot_text.__help)  
-        bot.register_next_step_handler(message, foo)
-        
-  
+        bot.send_message(message.chat.id, bot_text.__start_message)
+    if  message.text.lower() == "/check":
+        bot.send_message(message.chat.id, bot_text.__check)  
+        bot.register_next_step_handler(message, foo)   
+
 def foo(message):
     dict_with = je.send_request_to_dict(message.text)
     for obj in dict_with:
